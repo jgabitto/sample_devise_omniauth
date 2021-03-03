c04933fda8b5212c1ead2ce2e41f2674965670f3
Jorge Gabitto <jgabitto1792@gmail.com>
2021-03-03 16:33:08 -0600

    added stuff to application.rb and application_controller

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,6 @@ module DeviseSignInOmni
     config.api_only = true
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
+    config.middleware.use ActionDispatch::Flash
   end
 end
