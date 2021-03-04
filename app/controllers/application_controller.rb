aa8da82489eeaf9525ee7b95cb3bcfab5aa52441
Jorge Gabitto <jgabitto1792@gmail.com>
2021-03-03 18:09:59 -0600

    added skip_before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-    include ActionController::RequestForgeryProtection
-    protect_from_forgery with: :null_session, prepend: true
-
+    # include ActionController::RequestForgeryProtection
+    # protect_from_forgery with: :null_session, prepend: true
+    skip_before_action :verify_authenticity_token
 end
