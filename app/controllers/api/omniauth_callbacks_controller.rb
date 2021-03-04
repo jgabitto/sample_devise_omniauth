bd05d1f6adaaeae0edc0116b73e41c48ebd87e99
Jorge Gabitto <jgabitto1792@gmail.com>
2021-03-03 18:44:22 -0600

    added line to controller

--- app/controllers/api/omniauth_callbacks_controller.rb
+++ app/controllers/api/omniauth_callbacks_controller.rb
@@ -1,11 +1,13 @@
 class Api::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     skip_before_action :verify_authenticity_token
     respond_to :json
-    
+
     def github
         @user = User.from_omniauth(request.env["omniauth.auth"])
         p @user
-        render json: @user
+        sign_in_and_redirect @user, event: :authentication
+
+        # render json: @user
         # if @user.persisted?
         #     sign_in_and_redirect @user, event: :authentication
         # else
