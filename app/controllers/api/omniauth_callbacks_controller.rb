d59e31023f5977fbc45816ab61dc42b28aef4943
Jorge Gabitto <jgabitto1792@gmail.com>
2021-03-04 13:25:59 -0600

    added passthru

--- app/controllers/api/omniauth_callbacks_controller.rb
+++ app/controllers/api/omniauth_callbacks_controller.rb
@@ -2,12 +2,15 @@ class Api::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # include ActionController::RequestForgeryProtection
     respond_to :json
 
+    def passthru
+    end
+
     def github
         @user = User.from_omniauth(request.env["omniauth.auth"])
         p @user
-        sign_in_and_redirect @user, event: :authentication
+        # sign_in_and_redirect @user, event: :authentication
 
-        # render json: @user
+        render json: @user
         # if @user.persisted?
         #     sign_in_and_redirect @user, event: :authentication
         # else
