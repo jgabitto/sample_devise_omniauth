13e222a4d1878514b19a2d2b464ce5ed33f2138b
Jorge Gabitto <jgabitto1792@gmail.com>
2021-03-03 19:15:11 -0600

    added protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::API
     # include ActionController::RequestForgeryProtection
-    # protect_from_forgery with: :null_session, prepend: true
     # skip_before_action :verify_authenticity_token
     include ActionController::Cookies
     include ActionController::RequestForgeryProtection
 
-    protect_from_forgery with: :exception
+    protect_from_forgery with: :null_session, prepend: true
+    # protect_from_forgery with: :exception
 end
